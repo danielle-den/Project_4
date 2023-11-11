4a643b44f0ed1467ab47993de32ac1fbb5d47bfd
danielle-den <143743140+danielle-den@users.noreply.github.com>
2023-11-11 15:29:48 -0500

    Multithreading encode

--- main.cpp
+++ main.cpp
@@ -1,51 +1,66 @@
 #include <fstream>
+#include <map>
+#include <vector>
 #include <iostream>
 #include <list>
-#include <unordered_map>
-#include <string>
+#include <thread>
+#include <pthread.h>
 
 using namespace std;
 
-// Creates the encoded and mapping structures
-void setup(fstream &file, unordered_map<string, int>& wordToIDMap, list<string> &wordList, list<int> &encodedData) {
-    string word;
-    int id = 1;
-
-    while (file >> word) {
-        if (wordToIDMap.find(word) == wordToIDMap.end()) {
-            wordToIDMap[word] = id;
-            id++;
-        }
-        wordList.push_back(word);
+//
+void encode(map<string, int>& mapping, int en, int end){
+  // Get the mapping and store it in the library
+  int count = 0;
+  for(auto i = mapping.begin(); count < int(mapping.size()); i++, count++){
+    if(count < end and count >= en){         // For each thread, only operate on the section of data dedicated for you
+      i->second += ++en;
+      cout << i->first << " " << i->second << endl;
     }
-    file.close();
+  }
 
-    // Display the word-to-ID mapping
-    for (const auto &pair : wordToIDMap) {
-        cout << pair.first << " " << pair.second << endl;
-    }
-
-    // Set the encodedData properly
-    for (const string &word : wordList) {
-        encodedData.push_back(wordToIDMap[word]);
-    }
+  return;
 }
 
-int main() {
-    string file_name;
-    unordered_map<string, int> wordToIDMap;
-    list<string> wordList;
-    list<int> encodedData;
+// Creates the encoded and mapping structures
+void setup(fstream &file, map<string, int>& mapping, list<string> &data, list<int> &encoded_data){
+  string read;
+  vector<thread> threads;
+  int en = 0;
+
+  while(file >> read){
+    if(mapping.find(read) == mapping.end()){    // Check to see if we have already read this word
+      mapping[read] = 0;
+    }
+
+    data.push_back(read);
+  }
+  file.close();
+
+  threads.push_back(thread(encode, ref(mapping), en, int(mapping.size()/2)));                       // First thread goes from beginning to middle
+  threads.push_back(thread(encode, ref(mapping), mapping.size()/2, int(mapping.size())));          // Second thread goes from middle to end
+  threads[0].join(); threads[1].join()  ;
+
+
+  // Set the encoded_data properly
+  for(auto i = data.begin(); i != data.end(); i++){
+    encoded_data.push_back(mapping[*i]);
+  }
+}
+
+
+int main(){
+    string file_name, read;
+    map<string, int> mapping;
+    list<string> data;                       list<int> encoded_data;
 
     cout << "file name: ";
     cin >> file_name;
 
     fstream file(file_name);
-    if (file.good()) {
-        setup(file, wordToIDMap, wordList, encodedData);
-    } else {
-        cout << "Unable to open file. Exiting program." << endl;
-    }
+    if(file.good()) setup(file, mapping, data, encoded_data);
+    else cout << "Unable to open file. Exiting program." << endl;
+
 
     return 0;
 }
