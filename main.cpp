8558da92c6ab9a4304fe2530f105596dd9a4a97d
danielle-den <143743140+danielle-den@users.noreply.github.com>
2023-11-12 22:20:08 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -24,12 +24,14 @@ void print(vector<int> &vec){
  *  type => Decides if this is a prefix scan or a single item scan. Can assume value of 1 or 0
  *
 /*/
+
+
 void Query(int type, string prefix,map<string, int>& mapping, vector<int> &Vals, map<int,vector<int>> &OrderData){
   map<string, int>::iterator itr_mapping;
   vector<int> lookupVals;
   Vals.clear();
 
-//Process data if valid request
+  //Process data if valid request
   if(type == 1 || type == 0){
     if(type == 1){
       //get the value associated with the input word
@@ -84,7 +86,7 @@ void vanilla(fstream &file, list<string> &data){
   }
 }
 
-// Encode using the delta algorithm
+// Encode using the delta algorithm. No SIMD
 void delta_encode(vector<int>& data, int start, int end) {
   if(start == 0) start = 1;
     for(; start < end; start++) {
@@ -92,69 +94,143 @@ void delta_encode(vector<int>& data, int start, int end) {
     }
 }
 
-// Decode using the delta algorithm
-vector<int> delta_decode(const vector<int>& compressed) {
-    vector<int> data;
-    if(compressed.empty()) return data;
+// Delta encoding using SIMD
+void delta_encode_simd(vector<int>& data, int start, int end){
+  if(data.empty()) return; // Nothing to encode in an empty vector.
 
-    data.push_back(compressed[0]); // First value remains the same
-    for(int i = 1; i < int(compressed.size()); i++) {
-        int original_value = data[i - 1] + compressed[i];
-        data.push_back(original_value);
-    }
-    return data;
+  // Use SSE2 intrinsics for SIMD processing
+  __m128i prev;
+  if(start == 0){
+    prev = _mm_set1_epi32(data[0]);                     // The first index stays the same
+    start = 1;
+  }
+
+  for(size_t i = start; i < size_t(end); i += 4){
+    __m128i current = _mm_loadu_si128(reinterpret_cast<const __m128i*>(&data[i]));
+    __m128i delta = _mm_sub_epi32(current, prev);
+
+    // Store the deltas back in the original data
+    _mm_storeu_si128(reinterpret_cast<__m128i*>(&data[i]), delta);
+
+    prev = current;
+  }
 }
 
-// Compress using the variable byte algorithm
-vector<uint8_t> variable_byte_encode(int value) {
-    std::vector<uint8_t> bytes;
-    while (value >= 128) {
-        bytes.push_back(static_cast<uint8_t>(value & 0x7F) | 0x80);
-        value >>= 7;
-    }
-    bytes.push_back(static_cast<uint8_t>(value));
-    return bytes;
+// Delta decoding with SIMD
+void delta_decode_simd(vector<int>& data) {
+  if(data.empty()) return; // Nothing to decode in an empty vector.
+
+  // Use SSE2 intrinsics for SIMD processing
+  __m128i prev = _mm_set1_epi32(data[0]);
+
+  for(size_t i = 1; i < data.size(); i += 4){
+    __m128i delta = _mm_loadu_si128(reinterpret_cast<const __m128i*>(&data[i]));
+    __m128i current = _mm_add_epi32(prev, delta);
+
+    // Store the current values back in the original data
+    _mm_storeu_si128(reinterpret_cast<__m128i*>(&data[i]), current);
+
+    prev = current;
+  }
 }
 
-// Uncompress using the variable_byte algorithm
-inline int variable_byte_decode_simd(const std::vector<uint8_t>& bytes){
-  __m128i result = _mm_setzero_si128();
-  __m128i mask = _mm_set1_epi8(0x7F);
+// Delta decoding without SIMD
+void delta_decode(vector<int>& data) {
+  if(data.empty()) return; // Nothing to decode in an empty vector.
 
-  int shift = 0;
-  size_t i = 0;
+  int prev = data[0]; // Initialize with the first element.
 
-  while(i < bytes.size()){
-    __m128i byte_data = _mm_set1_epi8(bytes[i]);
-    __m128i shift_data = _mm_slli_si128(byte_data, shift);
-    result = _mm_or_si128(result, _mm_and_si128(shift_data, mask));
-    shift += 7;
-    i++;
+  for(size_t i = 1; i < data.size(); ++i){
+    int delta = data[i];
+    data[i] = prev + delta; // Decode the value.
+    prev = data[i];
+  }
+}
 
-    if((bytes[i - 1] & 0x80) == 0){
-      break;
+// Single word querying
+void query(const string& value, const map<string,int> &mapping, vector<int>& encoded_data, bool SIMD, bool prefix = false){
+  if(not prefix){                     // If we're not doing prefix search
+    auto find = mapping.find(value);
+    if(find == mapping.end()){
+      cout << "value does not exist" << endl;
+      return;
     }
   }
 
-  int decoded_value;
-  _mm_storeu_si128(reinterpret_cast<__m128i*>(&decoded_value), result);
-  return decoded_value;
+  // Get the decode
+  auto time_start = std::chrono::high_resolution_clock::now();
+  if(SIMD && not prefix) delta_decode_simd(encoded_data);           // Only decode from this function if we are doing normal query
+  if(not SIMD && not prefix) delta_decode(encoded_data);            // Use the regular decoding
+
+  for(int i = 0; i < int(encoded_data.size()); i++){
+    // if(encoded_data[i] == find->second) cout << i+1 << ", ";
+  }
+
+  cout << endl;
+  auto time_end = std::chrono::high_resolution_clock::now();
+  auto duration = chrono::duration_cast<chrono::milliseconds>(time_end - time_start);
+  cout << endl;
+  cout << "time: " << (duration).count() << endl;
+}
+
+// Prefix query
+void query_prefix(const string& value, const map<string,int> &mapping, vector<int>& encoded_data, bool SIMD){
+  // Find matches
+  cout << "Beginning prefix query timer" << endl;
+  auto time_start = std::chrono::high_resolution_clock::now();
+  if(SIMD){
+    cout << "SIMD" << endl;
+    delta_decode_simd(encoded_data);
+  }else{
+    delta_decode(encoded_data);
+  }
+
+  // Go through mapping map to find possible matches
+  for(auto i = mapping.begin(); i != mapping.end(); i++){
+    if(i->first.substr(0, value.size()) == value) {         // Once we find a match
+      cout << i->first << endl;
+      query(i->first, mapping, encoded_data, true);         // Use the original query function to find the value
+      cout << endl;
+    }
+  }
+
+  auto time_end = std::chrono::high_resolution_clock::now();
+  auto duration = chrono::duration_cast<chrono::milliseconds>(time_end - time_start);
+  cout << endl;
+  cout << "Prefix query ending. Duration: " << (duration).count() << endl;
+}
+
+// Export data
+void export_file(const map<string,int>& mapping, vector<int>& encoded_data){
+  ofstream encoded_file, mapping_file;
+
+  encoded_file.open("encoded_data.txt");
+  mapping_file.open("mapping.txt");
+
+  for(auto i = mapping.begin(); i != mapping.end(); i++){
+    mapping_file << i->first << " " << i->second << "\n";
+  }
+
+  for(auto i = encoded_data.begin(); i != encoded_data.end(); i++){
+    encoded_file << *i << "\n" ;
+  }
+
+  encoded_file.close(); mapping_file.close();
+
+  return;
 }
 
 // Creates the encoded and mapping structures
-void setup(fstream &file, map<string, int>& mapping, vector<int> &encoded_data){
+void setup(fstream &file, map<string, int>& mapping, vector<int> &encoded_data, bool SIMD){
   string read;
   list<thread> threads;
   vector<int> compressed, uncompressed;
   int numThreads = 2; int count = 0;
-  char technique;
 
   cout << "Enter the number of threads to use for encoding, between 1 and " <<
   std::thread::hardware_concurrency() << ": ";
   cin >> numThreads;
-  cout << "Enter the letter 'd' to use delta encoding, or the letter 'v' to use " <<
-  "variable byte encoding: ";
-  cin >> technique;
+  cout << "Reading data. Operation may take up to 5 minutes." << endl;
 
   while(file >> read){
     // Add the data without duplicates
@@ -175,47 +251,27 @@ void setup(fstream &file, map<string, int>& mapping, vector<int> &encoded_data){
     int start = i * chunkSize;
     int end = (i == numThreads - 1) ? int(mapping.size()) : (i + 1) * chunkSize;
 
-    threads.push_back(thread(delta_encode, ref(encoded_data), start, end));
-    // else threads.push_back(thread(variable_byte_encode, ref(encoded_data), start, end));
+    if(not SIMD) threads.push_back(thread(delta_encode, ref(encoded_data), start, end));
+    else threads.push_back(thread(delta_encode_simd, ref(encoded_data), start, end));
   }
 
   for(auto i = threads.begin(); i != threads.end(); i++){
     i->join();
   }
   auto time_end = std::chrono::high_resolution_clock::now();
-  auto duration = (time_end - time_start);
+  auto duration = chrono::duration_cast<chrono::seconds>(time_end - time_start);
   cout << endl;
-  cout << "time: " << (duration).count() << endl;
-
-  /*cout << "Threads done" << endl;
-
-  // Further reduce
-  cout << "starting encoding" << endl;
-  auto time_start = std::chrono::high_resolution_clock::now();
-
-  if(technique == 'd'){
-    compressed = delta_encode(encoded_data);
-    // uncompressed = delta_decode(compressed);
-  }else if(technique == 'v'){
-    vector<vector<uint8_t> >variable_encode;
-    for(int i = 0; i < int(encoded_data.size()); i++){             // Encode the data
-      variable_byte_encode(encoded_data[i]);
-    }
-  }
-
-
-    // print(encoded_data); cout << endl;
-    // print(compressed); cout << endl;
-    // print(uncompressed);*/
+  cout << "Threads stopped. Encoding duration: " << (duration).count() << endl;
 }
 
 
 int main(){
     string file_name, read;
+    bool query_type = 0; bool SIMD = 0;
     map<string, int> mapping;
     list<string> data;    vector<int> encoded_data;
 
-    cout << "file name: ";
+    cout << "\nfile name: ";
     cin >> file_name;
 
     fstream file(file_name);
@@ -223,11 +279,24 @@ int main(){
       cout << "Enter y for dictionary encoding and n for regular: ";
       cin >> file_name;
 
-      if(file_name == "y") setup(file, mapping, encoded_data);
+      if(file_name == "y") {
+        cout << "Enter 1 to use SIMD, other for no SIMD: ";
+        cin >> SIMD;
+        setup(file, mapping, encoded_data, SIMD);
+        // export_file(mapping, encoded_data);
+        while(true){
+          cout << "Enter 1 for prefix query and 0 for regular query: ";
+          cin >> query_type;
+          cout << "enter value to search: ";
+          cin >> file_name;
+
+          if(not query_type) query(file_name,mapping, encoded_data, SIMD, query_type);    // Variable "file_name", is being reused as the variable to find out the query string
+          else query_prefix(file_name, mapping, encoded_data, SIMD);
+        }
+      }
       else vanilla(file, data);
     }
     else cout << "Unable to open file. Exiting program." << endl;
-
 
     return 0;
 }
